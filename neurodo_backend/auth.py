a9eef5244ffbe589d616e1a393d3f107bebc8b2b
iremtektas <tektasirem13@gmail.com>
2025-05-07 22:02:16 +0300

    register endpoint hazırlandı

---
+++ neurodo_backend/auth.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models, schemas, crud
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/register", response_model=schemas.UserOut)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db, user)
--- neurodo_backend/main.py
+++ neurodo_backend/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from database import Base, engine
-
+from auth import router as auth_router
 from models import User
 
 app = FastAPI()
 
-# !!! Bu satır tabloları yaratır
+# Tabloları yarat
 Base.metadata.create_all(bind=engine)
+
+# Router’ları bağla
+app.include_router(auth_router, prefix="/auth")
 
 @app.get("/")
 async def root():
