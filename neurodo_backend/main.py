127cdecd1e2d24f2b1ab8fc4ea19b85ee4cf62e1
iremtektas <tektasirem13@gmail.com>
2025-07-09 20:24:20 +0300

    Enable Swagger UI in production

--- neurodo_backend/main.py
+++ neurodo_backend/main.py
@@ -5,7 +5,10 @@ from src.api.v1.router import api_router
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
-    
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
 )
+
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
