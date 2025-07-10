faa3c390f7cdd89c171aa587fb2a35a01231c1f4
iremtektas <tektasirem13@gmail.com>
2025-07-10 14:52:19 +0300

    no message

--- neurodo_backend/main.py
+++ neurodo_backend/main.py
@@ -1,26 +1,7 @@
-print("✅ Starting FastAPI App")
-
 from fastapi import FastAPI
-from src.core.config import settings
-from src.api.v1.router import api_router
 
+app = FastAPI()
 
-from src.core.database import Base, engine
-
-
-# Base.metadata.create_all(bind=engine)
-
-
-print("✅ Imports successful")
-
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    version=settings.VERSION,
-    docs_url="/docs",
-    redoc_url="/redoc",
-    openapi_url="/openapi.json"
-)
-
-app.include_router(api_router, prefix=settings.API_V1_STR)
-
-print("✅ FastAPI app created")
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Railway!"}
